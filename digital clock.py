f57e217001cc5c0da6aa0cb2c5aa836225f4df7c
ROHIT RAMESH GEDAM <100556673+rohitv315@users.noreply.github.com>
2023-04-11 10:07:03 +0530

    Create digital clock.py

---
+++ digital clock.py
@@ -0,0 +1,17 @@
+from tkinter import*
+from tkinter.ttk import*
+
+from time import strftime
+
+root = Tk()
+root.title("clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    Label.config(text=string)
+    Label.after(1000,time)
+Label = Label(root,font=("ds-digital",80),background="black",foreground="red")
+Label.pack(anchor="center")
+time()
+
+mainloop()
